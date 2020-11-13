103fd92e7d1217a6bfad1d255c885a08a212be7f
nicholas husher <nhusher@gmail.com>
2020-11-13 15:27:17 -0500

    Try to write a deploy script. Fail.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-npm build
+# Standard paranoia.
+set -euo pipefail
 
-echo "Fetching master npm token from vault"
-$NPM_TOKEN = "${vault read secret/services/npm/token}"
-echo -n "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+echo "Building selectstar..."
+npm run build
 
-echo "Publishing to npm"
+#echo "Fetching master npm token from vault..."
+#NPM_TOKEN="$(vault read --field=auth secret/services/npm/token)"
+#echo -n "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+
+echo "Publishing to npm..."
 npm publish
 
-echo "Deleting npm token"
+echo "Deleting npm token..."
 rm .npmrc
+
+echo "Done!"
